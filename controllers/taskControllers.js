f4269eb465413699e00ad723351424859b7b0570
Ruweydo10 <rumaanaxmad@gmail.com>
2024-11-04 04:31:34 -0800

    Commits

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -6,12 +6,14 @@ const { copyFileSync } = require('fs');
 const path = require('path');
 
 exports.getTasks = (req, res) => {
+
     const tasks = readTasksFromFile();
     res.writeHead(200, { 'content-type': 'application/json'})
     res.end(JSON.stringify(tasks))
 }
 
 exports.createTask = (req, res) => {
+
     const form = new IncomingForm();
     form.parse(req, (err, fields, files) => {
         if(err) {
@@ -19,15 +21,21 @@ exports.createTask = (req, res) => {
             res.end(JSON.stringify({
                 message: 'Error parsing form'
             }))
+
             return;
+
         }
 
+
         if (!fields.title) {
+
             res.writeHead(400, { 'content-type': 'application/json'});
             res.end(JSON.stringify({
                 message: 'Title is required'
             }))
+
             return;
+            
         }
 
         const image = files.image ? files.image[0] : null;
